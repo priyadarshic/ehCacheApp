a33837a1f099b7090817723a44cd8f9aee5d917c
priyadarshic <priyadarshi.c@gmail.com>
2022-08-02 16:28:34 +0530

    EnCache use in application

---
+++ src/main/java/com/ehcache/start/service/EmployeeManager.java
@@ -0,0 +1,27 @@
+package com.ehcache.start.service;
+
+import java.util.HashMap;
+
+import com.ehcache.start.model.Employee;
+import lombok.SneakyThrows;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeManager
+{
+    static HashMap<Long, Employee> db = new HashMap<>();
+
+    static {
+        db.put(1L, new Employee(1L, "Alex", "Gussin"));
+        db.put(2L, new Employee(2L, "Brian", "Schultz"));
+    }
+
+    @SneakyThrows
+    @Cacheable(cacheNames="employeeCache", key="#id")
+    public Employee getEmployeeById(Long id) {
+        System.out.println("Getting employee from DB...");
+        Thread.sleep(3*1000);
+        return db.get(id);
+    }
+}
